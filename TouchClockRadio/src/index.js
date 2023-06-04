fbb0b0aa46b7a29be53fbcbf3b89d5063f000dda
technocrazy8 <jevan2001@gmail.com>
2023-06-03 17:10:29 -0700

    another package

--- TouchClockRadio/src/index.js
+++ TouchClockRadio/src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { TopNav } from '../src/navigation/TopNav';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
+// import { ThemeProvider } from './context/Theme';
 
 
 const Index = () => {
     return (
-        <SafeAreaProvider style={{marginTop:40}}>
-            <StatusBar style="auto" />
-            <TopNav />
-        </SafeAreaProvider>
+        // <ThemeProvider>
+            <SafeAreaProvider style={{marginTop:40}}>
+                <StatusBar style="auto" />
+                <TopNav />
+            </SafeAreaProvider> 
+        // </ThemeProvider>
     )
 }
 export default Index;
